d9010b128b8994cba1b6779629cf9922682d2c30
Fedhii <fedhiigobena@gmail.com>
2020-07-16 13:05:07 -0500

    Finished middleware hw

---
+++ Middleware and Thunks/middleware_hw/frontend/todo_redux.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+
+  // Phase 1
+  // store.dispatch = addLoggingToDispatch(store);
+
+  // Phase 2
+ // store = applyMiddlewares(store, addLoggingToDispatch)
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+/* Phase 1
+
+const addLoggingToDispatch = (store) => {
+  const OGDispatch = store.dispatch;
+  return(action) => {
+    console.log(store.getState());
+    console.log(action);
+    OGDispatch(action);
+    console.log(store.getState());
+  }
+}
+*/
+
+/* Phase 2
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+};
+*/
