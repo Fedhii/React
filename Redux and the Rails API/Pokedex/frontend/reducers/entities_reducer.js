a90e24978a59882a7a7518e9fdafd4fd263fef39
Fedhii <fedhiigobena@gmail.com>
2020-07-23 12:05:55 -0500

    Finished Pokedex

--- Redux and the Rails API/Pokedex/frontend/reducers/entities_reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import pokemonReducer from './pokemon_reducer'
-
-export default combineReducers({
-    pokemon: pokemonReducer,
-})
--- Redux and the Rails API/Pokedex/frontend/reducers/selectors.js
+++
@@ -1,1 +0,0 @@
-export const selectAllPokemon = state => Object.values(state.entities.pokemon)
